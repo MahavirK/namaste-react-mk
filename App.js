1afe4e1829fd3771f95c7503a56197ee00f7dcbc
MahavirK <mahavir.kathed.1@gmail.com>
2023-05-20 18:48:17 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world from React!🚀"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
