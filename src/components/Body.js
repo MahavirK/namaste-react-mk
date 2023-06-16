bfad6077b1125966670a278deb1d613c481328ca
MahavirK <mahavir.kathed.1@gmail.com>
2023-06-16 09:51:48 +0530

    Day 5 Code

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,10 +2,15 @@ import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 import { useState } from "react";
 
+const searchRestaurants = (serachText) => {
+  return resList.filter((res) =>
+    res.data.name.toLowerCase().includes(serachText.toLowerCase())
+  );
+};
+
 const Body = () => {
-  const arr = useState(resList);
-  const [restaurantList, setRestaurantList] = arr;
-  console.log(arr);
+  const [restaurantList, setRestaurantList] = useState(resList);
+  const [serachText, setSerachText] = useState("");
   return (
     <div className="body">
       <div className="filter">
@@ -19,6 +24,26 @@ const Body = () => {
           }}
         >
           Top Rated Restaurant
+        </button>
+      </div>
+      <div className="search">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search for a restaurant"
+          value={serachText}
+          onChange={(e) => {
+            setSerachText(e.target.value);
+          }}
+        ></input>
+        <button
+          className="search-btn"
+          onClick={() => {
+            const searchResult = searchRestaurants(serachText);
+            setRestaurantList(searchResult);
+          }}
+        >
+          Search Restaurant
         </button>
       </div>
       <div className="res-container">
